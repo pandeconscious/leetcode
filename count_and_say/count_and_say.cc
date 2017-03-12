88f86d64e89e5e82b03c6e0992dc49571ff9a9e4
Harshit Pande <pandeconscious@gmail.com>
2017-03-12 14:54:51 +0530

    count and say - no special solution is needed - the most simple straightfroward solution works

---
+++ count_and_say/count_and_say.cc
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+	if(n == 1)
+		return "1";
+	if(n == 2)
+	    return "11";
+	
+	string str = "11";
+	for(int j = 3; j <= n; j++){
+		string temp = "";
+		int n = str.size();
+		int count = 1;
+		for(int i = 1; i < n; i++){
+			if(str[i] == str[i-1]){
+				count++;
+			}
+			else{
+			    stringstream ss;
+                ss << count;
+				temp += ss.str();
+				temp += str[i-1];
+				count = 1;
+			}
+		}
+		stringstream ss;
+	        ss << count;
+		temp += ss.str();
+		temp += str[n-1];
+		str = temp;
+
+	}
+	return str;
+    }
+};
